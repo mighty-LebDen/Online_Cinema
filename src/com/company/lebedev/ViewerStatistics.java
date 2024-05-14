4868ffb629e95373b1a60d61b7cda7273364b297
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-14 15:55:14 +0900

    Add realization ViewerStatistics

--- src/com/company/lebedev/ViewerStatistics.java
+++ src/com/company/lebedev/ViewerStatistics.java
@@ -1,4 +1,13 @@
 package com.company.lebedev;
 
+import java.util.List;
+
 public class ViewerStatistics {
+  public static double averageAge(List<Viewer> listOfViewers) {
+    int sumAges = 0;
+    for(var viewer : listOfViewers) {
+      sumAges += viewer.getAge();
+    }
+    return (double) sumAges / listOfViewers.size();
+  }
 }
