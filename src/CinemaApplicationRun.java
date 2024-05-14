107698c9ae233ccd8b192a76fca4ecb0462132d6
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-14 16:27:02 +0900

    update CinemaApplication update creating of viewers, add initialization of cinema

--- src/CinemaApplicationRun.java
+++ src/CinemaApplicationRun.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class CinemaApplicationRun {
   public static void main(String[] args) {
-    Viewer viewer1 = new Viewer("viewer1", 10, 5);
-    Viewer viewer2 = new Viewer("viewer2", 20, 6);
-    Viewer viewer3 = new Viewer("viewer3", 30, 7);
-    Viewer viewer4 = new Viewer("viewer4", 40, 8);
-    Viewer viewer5 = new Viewer("viewer5", 50, 9);
+    Cinema cinema = new Cinema("film1", "genre 1", 2);
+    Cinema cinema1 = new Cinema("film2", "genre 2", 2);
+    Cinema cinema2 = new Cinema("film3", "genre 3", 2);
+    Cinema cinema3 = new Cinema("film4", "genre 4", 2);
+    List<Cinema> cinemaList = new ArrayList<>(){{
+      add(cinema);
+      add(cinema1);
+      add(cinema2);
+      add(cinema3);
+    }};
+    Viewer viewer1 = new Viewer("viewer1", 10, cinemaList);
+    Viewer viewer2 = new Viewer("viewer2", 20, cinemaList);
+    Viewer viewer3 = new Viewer("viewer3", 30, cinemaList);
+    Viewer viewer4 = new Viewer("viewer4", 40, cinemaList);
+    Viewer viewer5 = new Viewer("viewer5", 50, cinemaList);
     List<Viewer> viewerList = new ArrayList<>();
     viewerList.add(viewer1);
     viewerList.add(viewer2);
